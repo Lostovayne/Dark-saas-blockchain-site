a75f25ff7716d197491c353a91793b2c5231a08d
LostoVayne <moises.barrera.dev@gmail.com>
2024-09-12 00:04:40 -0300

    agrega completamente la seccion FeatureCardsSection

--- src/sections/FeaturesCards.tsx
+++ src/sections/FeaturesCards.tsx
@@ -1,27 +1,73 @@
+const cardData = [
+  {
+    image: "",
+    title: "",
+    description: "",
+    color: "",
+  },
+  {  image: "",
+      title: "",
+      description: "",
+      color: "",},
+  {  image: "",
+      title: "",
+      description: "",
+      color: "",},
+  {  image: "",
+      title: "",
+      description: "",
+      color: "",},
+];
 
 const FeaturesCardsSection = () => {
   return (
-    <section className="py-24" >
+    <section className="py-24">
       <div className="container">
-        <h2 className="font-heading font-black text-4xl text-center" >Discover the future blockhain with Blockforge</h2>
-        <div>
-          <div className="relative z-0" >
-            <div className="absolute inset-0 bg-zinc-800 -z-10 rounded-2xl [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]" ></div>
-            <img src="/assets/images/pill.png" alt="Pill Image" />
-            <h3>Revolutionary Blockchain API</h3>
-            <p>
+        <h2 className="font-heading font-black text-4xl text-center">
+          Discover the future of blockhain with Blockforge
+        </h2>
+        <div className="mt-36">
+          <div className="relative z-0 p-8 max-w-xs group ">
+            <div className="absolute size-16 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10 blur-lg opacity-0 group-hover:opacity-100 transition duration-300"></div>
+
+            <div
+              className="absolute size-16 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10
+            group-hover:bg-fuchsia-400 transition duration-300
+            "
+            ></div>
+            <div className="absolute inset-0 bg-zinc-800 -z-10 rounded-2xl [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]"></div>
+            <div className="flex justify-center -mt-28">
+              <div className="inline-flex relative">
+                <div
+                  className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%]
+                [mask-image:radial-gradient(closest-side,black,transparent)]
+                "
+                ></div>
+                <img
+                  src="/assets/images/pill.png"
+                  alt="Pill"
+                  className="size-40 group-hover:-translate-y-6 transition duration-300"
+                />
+              </div>
+            </div>
+            <h3 className="font-heading font-black text-3xl mt-12">Revolutionary Blockchain API</h3>
+            <p className="text-lg text-zinc-400 mt-4">
               Effortlessly integrate and manage blockchain data with our cutting-edge API, designed for
               seamless connectivity{" "}
             </p>
-            <div className="flex">
-              <button>Learn More</button>
+            <div className="flex justify-between mt-12">
+              <button className="text-sm font-heading uppercase font-extrabold tracking-wider text-fuchsia-500">
+                Learn More
+              </button>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="size-6"
+                className="size-8 text-zinc-500 group-hover:text-zinc-300 transition duration-300
+                -translate-x-2 group-hover:translate-x-0
+                "
               >
                 <path
                   strokeLinecap="round"
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,11 +10,10 @@ export const HeroSection = () => {
           Introducing Blockforge
         </p>
         <h1 className="font-heading font-black text-5xl md:text-6xl lg:text-7xl text-center mt-4 max-w-3xl mx-auto">
-          The Future of Blockchaine is Here
+          The Future of Blockchaine is Here.
         </h1>
         <p className="text-center text-xl md:text-2xl mt-6 text-zinc-400 max-w-xl mx-auto ">
-          Blockforge is pioneering smart contract integrity with cutting-edge data
-          solutions.
+          Blockforge is pioneering smart contract integrity with cutting-edge data solutions.
         </p>
         <div className="flex justify-center mt-10">
           <CutCornerButton className="">Get Started</CutCornerButton>
@@ -30,31 +29,19 @@ export const HeroSection = () => {
             </div>
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
               <Circle className="absolute left-[200px] -top-[900px] ">
-                <img
-                  src="/assets/images/cube.png"
-                  alt="Cube 3d Image"
-                  className="size-[140px]"
-                />
+                <img src="/assets/images/cube.png" alt="Cube 3d" className="size-[140px]" />
               </Circle>
             </div>
 
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
               <Circle className="absolute  left-[200px] top-[270px]">
-                <img
-                  src="/assets/images/cuboid.png"
-                  alt="Cuboid 3d Image"
-                  className="size-[140px]"
-                />
+                <img src="/assets/images/cuboid.png" alt="Cuboid 3d" className="size-[140px]" />
               </Circle>
             </div>
 
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
               <Circle className="absolute -left-[600px] -top-[80px] ">
-                <img
-                  src="/assets/images/torus.png"
-                  alt="Torus 3d Image"
-                  className="size-[140px]"
-                />
+                <img src="/assets/images/torus.png" alt="Torus 3d" className="size-[140px]" />
               </Circle>
             </div>
 
@@ -64,11 +51,7 @@ export const HeroSection = () => {
               alt=""
             />
 
-            <img
-              src="/assets/images/icosahedron.png"
-              alt="Icosanhedron 3D Image"
-              className="w-[500px]"
-            />
+            <img src="/assets/images/icosahedron.png" alt="Icosanhedron 3D" className="w-[500px]" />
           </div>
         </div>
 
@@ -76,9 +59,7 @@ export const HeroSection = () => {
           <div className=" h-10 w-5 outline outline-[6px] outline-fuchsia-500/10 inline-flex justify-center pt-2 rounded-full">
             <div className="h-3 w-1 bg-fuchsia-500 rounded-full"></div>
           </div>
-          <p className="uppercase text-zinc-500 font-extrabold tracking-wider">
-            Scroll to learn more
-          </p>
+          <p className="uppercase text-zinc-500 font-extrabold tracking-wider">Scroll to learn more</p>
         </div>
       </div>
     </section>
