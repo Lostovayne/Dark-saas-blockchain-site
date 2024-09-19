2e9d3afe141140bb826ecb6665eb3bdac4b4132b
Lostovayne <moises.barrera.dev@gmail.com>
2024-09-19 02:57:07 -0300

    agrega nueva seccion LatestPosts

---
+++ src/components/LatestPosts.tsx
@@ -0,0 +1,23 @@
+export const LatestPosts = () => {
+  return (
+    <section>
+      <div className="container">
+        <h2>Your portal to everything blockchain.</h2>
+        <p>
+          Keep up with the newest trends, updates, and insights in the blockchain world, updated
+          weekly
+        </p>
+        <div>
+          <div>
+            <div>Technology</div>
+            <h3>Regularity Challenges Facing Blockchain</h3>
+            <p>
+              Understanding the regulatory landscape surrounding blockchain and what it means for
+              the future of the technology.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
