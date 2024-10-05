cc7bd97d68c2831d52cc5af3c7e5b16e458dc3b4
Lostovayne <moises.barrera.dev@gmail.com>
2024-10-05 18:32:22 -0300

    version de despliegue

--- src/sections/FeaturesCards.tsx
+++ src/sections/FeaturesCards.tsx
@@ -35,36 +35,36 @@ const cardData = [
 
 const FeaturesCardsSection = () => {
   return (
-    <section className='py-24 overflow-clip md:-mt-28'>
-      <div className='container'>
-        <h2 className='font-heading font-black text-4xl md:text-5xl lg:text-6xl text-center'>
+    <section className="py-24 overflow-clip md:-mt-28">
+      <div className="container">
+        <h2 className="font-heading font-black text-4xl md:text-5xl lg:text-6xl text-center">
           Discover the future of blockchain with Blockforge.
         </h2>
-        <div className='mt-36 lg:mt-48 flex'>
-          <div className='flex flex-none gap-8'>
+        <div className="mt-36 lg:mt-48 flex">
+          <div className="flex flex-none gap-8">
             {cardData.map(({ image, title, description, color }) => (
-              <Card key={title} className='max-w-xs md:max-w-md' pdbutton>
-                <div className='flex justify-center -mt-28'>
-                  <div className='inline-flex relative'>
-                    <div className='absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)]'></div>
+              <Card key={title} className="max-w-xs md:max-w-md" pdbutton>
+                <div className="flex justify-center -mt-28">
+                  <div className="inline-flex relative">
+                    <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)]"></div>
                     <img
                       src={image}
-                      alt='Pill'
-                      className='size-40 group-hover:-translate-y-6 transition duration-300'
+                      alt="Pill"
+                      className="size-40 group-hover:-translate-y-6 transition duration-300"
                     />
                   </div>
                 </div>
-                <h3 className='font-heading font-black text-3xl mt-12'>{title}</h3>
-                <p className='text-lg text-zinc-400 mt-4'>{description}</p>
+                <h3 className="font-heading font-black text-3xl mt-12">{title}</h3>
+                <p className="text-lg text-zinc-400 mt-4">{description}</p>
               </Card>
             ))}
           </div>
         </div>
 
-        <div className='flex justify-center mt-10'>
-          <div className='bg-zinc-950 inline-flex gap-4 p-2.5 rounded-full'>
+        <div className="flex justify-center mt-10">
+          <div className="bg-zinc-950 inline-flex gap-4 p-2.5 rounded-full">
             {cardData.map(({ title }) => (
-              <div key={title} className='size-2.5 bg-zinc-500 rounded-full cursor-pointer'></div>
+              <div key={title} className="size-2.5 bg-zinc-500 rounded-full cursor-pointer"></div>
             ))}
           </div>
         </div>
